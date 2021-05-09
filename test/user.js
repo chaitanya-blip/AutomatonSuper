f912de19e2527be92d8eb1ee9c515432d5fee64f
chaitanya-blip <chaitanya1997sai@gmail.com>
2021-05-09 14:44:10 +0530

    second commit

--- test/user.js
+++ test/user.js
@@ -1,16 +1,18 @@
 import supertest from "supertest";;
 import chai from "chai";
-import config from "../config";
+import url from "../config";
 import jsonschema from "../test_data/jsonschema/getuser.json";
+import jsonschema1 from "../test_data/jsonschema/postuser2.json"
+import jsonschema2 from "../test_data/jsonschema/putuser.json"
 import {expect} from "chai";
 import {it} from "mocha";
 chai.use(require ("chai-json-schema"));
-const request = supertest(config.baseurl);
+const request = supertest(url.baseurl);
 
 
 describe('/api/users/2', () => {
     it('GET user detail in page 2' ,async() => {
-        const response = await request.get(config.getUser)
+        const response = await request.get(url.getUser)
         
         .expect(200)
         .expect((res) => {
@@ -22,4 +24,44 @@ describe('/api/users/2', () => {
 
     });
 
+    describe('/api/users', () => {
+        it('Post create an user detail', async() => {
+            const response = await request.post(url.postUser)
     
+            .send ({
+                
+                    "name": "morpheus",
+                    "job": "leader",
+                
+            })
+            .expect(201)
+            .expect((res) => {
+                //console.log(res)
+                expect(res.body).to.be.jsonSchema(jsonschema1.valid_schema)
+            })
+            
+        });
+    
+    });
+
+
+    describe('api/users/2', () => {
+        it('Put update an user detail', async() => {
+            const response = await request.put(url.putUser)
+            .type('application/json')
+    
+            .send  ({
+                
+                    "name": "chaiz",
+                    "job": "leader",
+                
+            })
+            .expect(200)
+            .expect((res) => {
+                //console.log(res)
+                expect(res.body).to.be.jsonSchema(jsonschema2.valid_schema)
+            })
+            
+        });
+    
+    });
